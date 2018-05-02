0af17ce60d35b87d6530ec248af91365155d24b4
vavassoriluca <vavassoriluca@yahoo.com>
2018-05-02 10:15:14 +0200

    Bugs fixed, the bot works, but doesn't call the ML algorithm since now

--- TwitterBot/twitter-bot.py
+++ TwitterBot/twitter-bot.py
@@ -1,7 +1,9 @@
 import tweepy
 import json
 import time
+import re
 from watson_developer_cloud import AssistantV1
+from random import randint
 
 # Twitter Authentication
 
@@ -16,21 +18,29 @@ api = tweepy.API(auth, wait_on_rate_limit=True)
 watson_assistant = AssistantV1(username="ea4b98b5-732f-4de9-a3ef-91d3473eec9a", password="cFsmQGk5OAdK", version="2018-05-01")
 watson_assistant.set_http_config({'timeout': 100})
 
+def findurls(string):
+    urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', string)
+    result = []
+    for url in urls:
+        result.extend(url.split())
+    return result
+
 class MyStreamListener(tweepy.StreamListener):
 
     def on_direct_message(self, status):
 
         dm = status.direct_message
         response = watson_assistant.message(workspace_id="d788424d-8fcd-4371-8caf-a0669d58ac53", input={'text': dm['text']})
-        print(dm)
-        time.sleep(1)
-        if(response["intents"][0]['intent'] == 'url'):
+        time.sleep(randint(1,3))
+        if(dm['sender']['screen_name'] == 'FakeNewsWatson'):
+            return True
+        if(len(response["intents"]) > 0 and response["intents"][0]['intent'] == 'url'):
+            urls = findurls(dm['text'])
+            print(len(urls))
             self.api.send_direct_message(dm['sender']['id'], text="The algorithm to evaluate the fake news index is in progress.")
         else:
             self.api.send_direct_message(dm['sender']['id'], text=response['output']['text'][0])
         return True
-
-
 
 myStreamListener = MyStreamListener(api)
 myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
