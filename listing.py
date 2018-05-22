56798057319fde3c85d434399d6b4f2c8decb580
fsal <ang.sal@hotmail.com>
2018-05-22 03:15:46 +0200

    add blacklisting

--- listing.py
+++ listing.py
@@ -5,18 +5,21 @@ import tldextract as tld
 def get_fake_site_info(url):
     """
     :param url: string
-    :return: dict with keys: 'type', '2nd type', '3rd type', 'Source Notes (things to know?)'.
-             The latter keys don't have a value in most cases
+    :return: string obtained by joining a dict with keys: 'type', '2nd type', '3rd type', 'Source Notes (things to know?)'.
+             The latter keys don't have a value in most cases.
     """
     ext = tld.extract(url)
     clean_url = '.'.join(ext[1:])
     with open('blacklist.json') as jsonfile:
         fake_news_urls = json.load(jsonfile)
         try:
-            return fake_news_urls[clean_url]
+            return 'site characteristics: ' + ', '.join([value for value in fake_news_urls[clean_url].values() if value != ''])
         except:
-            return {'type': 'this site is not known for spreading fake news.',
-                    '2nd type': '',
-                    '3rd type': '',
-                    'Source Notes (things to know?)': ''
-                    }
+            return 'site characteristics: this site is not known for spreading fake news.'
+
+                # stub of dict in case it is needed
+                #{'type': 'this site is not known for spreading fake news.',
+                #    '2nd type': '',
+                #    '3rd type': '',
+                #    'Source Notes (things to know?)': ''
+                #    }
