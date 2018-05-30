2af2daccbfa39deb2e03865998b628b6b7b6d54f
fsal <ang.sal@hotmail.com>
2018-05-30 21:27:41 +0200

    run chatbot and webapp on the same file

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request as req
-from twitter_api import get_trends, get_embed_code
+from WebApp.twitter_api import get_trends, get_embed_code
+from kay import Kay
 
 app = Flask(__name__)
+evaluator = Kay()
+
 
 @app.route('/')
 def main():
@@ -12,8 +15,9 @@ def main():
 def request():
     if req.method == 'POST':
       result = req.form
+      score = evaluator.evaluate(result["url"])
       html = get_embed_code(result["url"])
-    return render_template('evaluation_second.html', html=html)
+    return render_template('evaluation_second.html', html=html, score=score)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    app.run(debug=True)
