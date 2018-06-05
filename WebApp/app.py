c0430268139f29aed044a6dd9a690d7251f3969a
vavassoriluca <vavassoriluca@yahoo.com>
2018-06-05 15:12:40 +0200

    dashboard

--- WebApp/app.py
+++ WebApp/app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    trends = get_trends()
+    #trends = get_trends()
+    trends = []
     return render_template('index_second.html', trends= trends)
 
 @app.route('/request', methods=['GET', 'POST'])
@@ -14,6 +15,14 @@ def request():
       result = req.form
       html = get_embed_code(result["url"])
     return render_template('evaluation_second.html', html=html)
+
+@app.route('/firm-db/login', methods=['GET', 'POST'])
+def login():
+    return render_template('firm-db/login.html')
+
+@app.route('/firm-db/dashboard', methods=['GET', 'POST'])
+def dashboard():
+    return render_template('firm-db/dashboard.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
