dceb9c3b7410e6cc8d3e6f3ef191c5e1c6836e57
vavassoriluca <vavassoriluca@yahoo.com>
2018-06-06 16:48:49 +0200

    Web App almost finished

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,26 +1,34 @@
 from flask import Flask, render_template, request as req
-from WebApp.twitter_api import get_trends, get_embed_code
-from kay import Kay
+#from WebApp.twitter_api import get_trends, get_embed_code
+from twitter_api import get_trends, get_embed_code
+import json
+import builtins
+#from kay import Kay
 
 app = Flask(__name__)
-evaluator = Kay()
+#evaluator = Kay()
 
 
 @app.route('/')
 def main():
-    #trends = get_trends()
-    trends = []
+    trends = get_trends()
     return render_template('index_second.html', trends= trends)
 
 @app.route('/request', methods=['GET', 'POST'])
 def request():
-    if req.method == 'POST':
-      result = req.form
-      score = evaluator.evaluate(result["url"])
-      html = get_embed_code(result["url"])
-    return render_template('evaluation_second.html', html=html, score=score)
+    if req.method == 'GET':
+      result = req.args
+      #score = evaluator.evaluate(result["url"])
+      score = {"tweet_url": 0, # string
+                "article_url": 0, # string
+                "page_quality": "lightbox-yellow", #string
+                "text_evaluation": "lightbox-red", # string
+                "source_reliability": "lightbox-green", #string
+                "final_score": 43   #integer in range [0:100]
+               }
+    html = get_embed_code(result["url"])
+    return render_template('evaluation_second.html', html=html, confidence=score["final_score"], page_q=score["page_quality"], text_ev=score["text_evaluation"], source=score["source_reliability"])
 
-<<<<<<< HEAD
 @app.route('/firm-db/login', methods=['GET', 'POST'])
 def login():
     return render_template('firm-db/login.html')
@@ -29,9 +37,17 @@ def login():
 def dashboard():
     return render_template('firm-db/dashboard.html')
 
+@app.route('/firm-db/user', methods=['GET', 'POST'])
+def user():
+    return render_template('firm-db/user.html')
+
+@app.route('/firm-db/table', methods=['GET', 'POST'])
+def table():
+    return render_template('firm-db/table.html')
+
+@app.route('/firm-db/notifications', methods=['GET', 'POST'])
+def notifications():
+    return render_template('firm-db/notifications.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
-=======
-#if __name__ == '__main__':
-#    app.run(debug=True)
->>>>>>> f05058054598fb1314ee27bd0b3b087931671935
